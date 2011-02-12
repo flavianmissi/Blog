f781d4b297fe0f19455f16c3ebb34f153b41de42
Flavia Missi <flaviamissi@gmail.com>
2011-02-12 20:06:13 -0200

    Created migration for HABTM association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110212180250) do
+ActiveRecord::Schema.define(:version => 20110212220022) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(:version => 20110212180250) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts_tags", :id => false, :force => true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
   end
 
   create_table "tags", :force => true do |t|
