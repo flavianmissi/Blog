cf1c13efbe1d103f5ef3eb663d8dd04b57b12b35
Flavia Missi <flaviamissi@gmail.com>
2011-02-12 18:27:36 -0200

    Added template for new tag

--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get index and assign :tags" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:tags)
   end
 
+  test "Should get new" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:tag)
+  end
 end
